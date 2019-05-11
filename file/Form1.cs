c862e6b5712fab9bb421e31c57bd9b8811d350a0
mahmoudjoe3 <mahmoud2017170415@cis.asu.edu.eg>
2019-05-11 15:08:19 +0200

    done

--- file/Form1.cs
+++ file/Form1.cs
@@ -39,117 +39,7 @@ namespace file
 
         }
         
-        private void ADD()
-        {
-            int rows = (table.RowCount)-1;
-            File.Delete(filePath);
-            for (int row = 0; row < rows; row++)
-            {
-                List<string> projects = new List<string>();
-                string id = "", name = "", gender = "", department = "";
-                int No_of_col = table.ColumnCount;
-
-                //int row = table.CurrentCell.RowIndex;
-                id = table.Rows[row].Cells[0].Value.ToString();
-                name = table.Rows[row].Cells[1].Value.ToString();
-                gender = table.Rows[row].Cells[2].Value.ToString();
-                department = table.Rows[row].Cells[3].Value.ToString();
-                for (int colindex = 4; colindex < No_of_col; colindex++)
-                {
-                    if (table.Rows[row].Cells[colindex].Value!= null)
-                    {
-                        string pro = table.Rows[row].Cells[colindex].Value.ToString();
-                        projects.Add(pro);
-                    }
-                }
-                student st = new student(id, name, gender, department, projects);
-                
-                //write in xml
-                if (!File.Exists(filePath))
-                {
-                    XmlWriter xw = XmlWriter.Create(filePath);
-                    xw.WriteStartDocument();
-
-                    xw.WriteStartElement("student");
-
-                    xw.WriteStartElement("id");
-                    xw.WriteString(st.id);
-                    xw.WriteEndElement();
-
-                    xw.WriteStartElement("name");
-                    xw.WriteString(st.name);
-                    xw.WriteEndElement();
-
-                    xw.WriteStartElement("gender");
-                    xw.WriteString(st.gender);
-                    xw.WriteEndElement();
-
-                    xw.WriteStartElement("department");
-                    xw.WriteString(st.department);
-                    xw.WriteEndElement();
-
-                    xw.WriteStartElement("projects");
-                    int i = 1;
-                    foreach (string pro in st.projects)
-                    {
-                        xw.WriteStartElement("project" + i);
-                        xw.WriteString(pro);
-                        xw.WriteEndElement();
-                        i++;
-                    }
-                    xw.WriteEndElement();
-
-                    xw.WriteEndElement();
-                    xw.WriteEndDocument();
-                    xw.Close();
-                    
-                }
-                else
-                {
-
-                    XmlDocument Doc = new XmlDocument();
-                    XmlElement student = Doc.CreateElement("student");
-                    XmlElement node;
-                    XmlElement nodein;
-
-                    node = Doc.CreateElement("id");
-                    node.InnerText = st.id;
-                    student.AppendChild(node);
-
-                    node = Doc.CreateElement("name");
-                    node.InnerText = st.name;
-                    student.AppendChild(node);
-
-                    node = Doc.CreateElement("gender");
-                    node.InnerText = st.gender;
-                    student.AppendChild(node);
-
-                    node = Doc.CreateElement("department");
-                    node.InnerText = st.department;
-                    student.AppendChild(node);
-
-                    int i = 1;
-                    node = Doc.CreateElement("project");
-                    foreach (string pro in st.projects)
-                    {
-                        nodein = Doc.CreateElement("project" + i);
-                        nodein.InnerText = pro;
-                        node.AppendChild(nodein);
-                        i++;
-                    }
-                    student.AppendChild(node);
-
-                    Doc.Load(filePath);
-                    XmlElement Root = Doc.DocumentElement;
-                    Root.AppendChild(student);
-                    Doc.Save(filePath);
-                }
-
-            }
-
-                MessageBox.Show("Edit is Done");
-            
-        }
+        
         // Aggregate functions
         private void sum_Click(object sender, EventArgs e)
         {
@@ -271,6 +161,117 @@ namespace file
 
 
         //function of noded xml
+        private void edit()
+        {
+            int rows = (table.RowCount)-1;
+            File.Delete(filePath);
+            for (int row = 0; row < rows; row++)
+            {
+                List<string> projects = new List<string>();
+                string id = "", name = "", gender = "", department = "";
+                int No_of_col = table.ColumnCount;
+
+                //int row = table.CurrentCell.RowIndex;
+                id = table.Rows[row].Cells[0].Value.ToString();
+                name = table.Rows[row].Cells[1].Value.ToString();
+                gender = table.Rows[row].Cells[2].Value.ToString();
+                department = table.Rows[row].Cells[3].Value.ToString();
+                for (int colindex = 4; colindex < No_of_col; colindex++)
+                {
+                    if (table.Rows[row].Cells[colindex].Value!= null)
+                    {
+                        string pro = table.Rows[row].Cells[colindex].Value.ToString();
+                        projects.Add(pro);
+                    }
+                }
+                student st = new student(id, name, gender, department, projects);
+                
+                //write in xml
+                if (!File.Exists(filePath))
+                {
+                    XmlWriter xw = XmlWriter.Create(filePath);
+                    xw.WriteStartDocument();
+
+                    xw.WriteStartElement("student");
+
+                    xw.WriteStartElement("id");
+                    xw.WriteString(st.id);
+                    xw.WriteEndElement();
+
+                    xw.WriteStartElement("name");
+                    xw.WriteString(st.name);
+                    xw.WriteEndElement();
+
+                    xw.WriteStartElement("gender");
+                    xw.WriteString(st.gender);
+                    xw.WriteEndElement();
+
+                    xw.WriteStartElement("department");
+                    xw.WriteString(st.department);
+                    xw.WriteEndElement();
+
+                    xw.WriteStartElement("projects");
+                    int i = 1;
+                    foreach (string pro in st.projects)
+                    {
+                        xw.WriteStartElement("project" + i);
+                        xw.WriteString(pro);
+                        xw.WriteEndElement();
+                        i++;
+                    }
+                    xw.WriteEndElement();
+
+                    xw.WriteEndElement();
+                    xw.WriteEndDocument();
+                    xw.Close();
+                    
+                }
+                else
+                {
+
+                    XmlDocument Doc = new XmlDocument();
+                    XmlElement student = Doc.CreateElement("student");
+                    XmlElement node;
+                    XmlElement nodein;
+
+                    node = Doc.CreateElement("id");
+                    node.InnerText = st.id;
+                    student.AppendChild(node);
+
+                    node = Doc.CreateElement("name");
+                    node.InnerText = st.name;
+                    student.AppendChild(node);
+
+                    node = Doc.CreateElement("gender");
+                    node.InnerText = st.gender;
+                    student.AppendChild(node);
+
+                    node = Doc.CreateElement("department");
+                    node.InnerText = st.department;
+                    student.AppendChild(node);
+
+                    int i = 1;
+                    node = Doc.CreateElement("project");
+                    foreach (string pro in st.projects)
+                    {
+                        nodein = Doc.CreateElement("project" + i);
+                        nodein.InnerText = pro;
+                        node.AppendChild(nodein);
+                        i++;
+                    }
+                    student.AppendChild(node);
+
+                    Doc.Load(filePath);
+                    XmlElement Root = Doc.DocumentElement;
+                    Root.AppendChild(student);
+                    Doc.Save(filePath);
+                }
+
+            }
+
+                MessageBox.Show("Edit is Done");
+            
+        }
         private void load_noded_xml()
         {
 
@@ -627,7 +628,7 @@ namespace file
                     st.Show();
                 }
                 else
-                    ADD();
+                    edit();
 
             }
             else
